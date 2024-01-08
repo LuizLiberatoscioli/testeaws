16cffbb928bf318d86c67154f2fcd1697e2c99b9
LuizLiberatoscioli <luizhat10@gmail.com>
2024-01-08 12:24:01 -0500

    new project

---
+++ src/main/java/com/aws/demo/controller/TesteApi.java
@@ -0,0 +1,12 @@
+package com.aws.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteApi {
+	
+	private String teste() {
+		return "Sucesso";
+	}
+
+}
