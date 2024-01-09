f8cc2db82f2043b85e95bb6f95a28fb312582636
LuizLiberatoscioli <luizhat10@gmail.com>
2024-01-09 18:57:45 -0500

    update and security

---
+++ src/main/java/com/aws/demo/controller/TesteApiController.java
@@ -0,0 +1,21 @@
+package com.aws.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteApiController {
+	
+	@GetMapping("/teste-api")
+	private String teste() {
+		return "Sucesso";
+	}
+	
+	@GetMapping("/teste-api-bem-vindo")
+	public String testeBemVindo(@RequestParam(name = "nome") String nome) {
+		return "Ola " +nome+ " ,seja muito bem vindo! ";
+		
+	}
+
+}
