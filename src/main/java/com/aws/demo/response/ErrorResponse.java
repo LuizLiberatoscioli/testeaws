0a7e36b7e82334e9a9ddb1d6f4b67e361193e8f2
LuizLiberatoscioli <luizhat10@gmail.com>
2024-02-09 12:26:56 -0500

    start create validations

---
+++ src/main/java/com/aws/demo/response/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.aws.demo.response;
+
+import java.util.Map;
+
+public class ErrorResponse {
+	
+	private String status; //400 - bad request
+	private Map<String ,String> erros; // campo // decricao
+
+}
